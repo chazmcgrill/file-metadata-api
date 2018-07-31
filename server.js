9c33d6ef7307cabe28434c17800a0744d0f111aa
chazmcgrill <charlietaylordev@gmail.com>
2018-07-31 12:25:31 +0100

    Added Multer & EJS / Created Metadata Route

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+// set multer storage
+const storage = multer.diskStorage({
+  destination: './public/uploads',
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+
+// Initilise upload
+const upload = multer({
+  storage: storage
+}).single('upfile')
+
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.render('index');
 });
 
-// app.post('/metadata', (res, req) => {
-
-// });
+app.post('/metadata', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.json({error: "error uploading file"})
+    } else {
+      const data = { 
+        name: req.file.originalname, 
+        type: req.file.mimetype, 
+        size: req.file.size 
+      };
+      res.render('index', { msg: "file uploaded", data });
+    }
+  })
+});
 
 const port = process.env.PORT || 8080;
 
